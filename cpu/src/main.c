3d2d39bd294ec700f95df2ea500ad96ede3f3df3
nicofishman <nicourifishman@gmail.com>
2024-04-10 18:05:02 +0000

    [CPU] - Lectura de config y creación de logs

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,8 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/hello.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/string.h>
 
 int main(int argc, char* argv[]) {
-    decir_hola("CPU");
+    // decir_hola("CPU");
+
+    t_log* logger = log_create("cpu.log", "CPU", 1, LOG_LEVEL_INFO);
+    t_config* config = config_create("cpu.config");
+
+    int puerto = config_get_int_value(config, "PUERTO_ESCUCHA_DISPATCH");
+
+    char* stringParaLogger = string_from_format("Hola, soy un cpu! Puerto: %d", puerto);
+    log_info(logger, stringParaLogger);
+
     return 0;
 }
