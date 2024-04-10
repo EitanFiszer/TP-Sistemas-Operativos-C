4b00bcf6454b0e9abc8a1960fa97c9a97158d4c3
nicofishman <nicourifishman@gmail.com>
2024-04-10 17:49:01 +0000

    Archivos de utils importados de tp0

---
+++ utils/src/utils/server.c
@@ -0,0 +1,109 @@
+#include "server.h"
+
+int iniciar_servidor(int puerto, t_log* logger)
+{
+	int socket_servidor;
+
+	struct addrinfo hints, *servinfo;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(NULL, puerto, &hints, &servinfo);
+
+	// Creamos el socket de escucha del servidor
+	socket_servidor = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
+	// Asociamos el socket a un puerto
+	bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
+
+	// Escuchamos las conexiones entrantes
+	int ret = listen(socket_servidor, SOMAXCONN);
+	if (ret == -1)
+	{
+		log_error(logger, "Error al escuchar las conexiones entrantes");
+	}
+
+	freeaddrinfo(servinfo);
+	log_trace(logger, "Listo para escuchar a mi cliente");
+
+	return socket_servidor;
+}
+
+int esperar_cliente(int socket_servidor)
+{
+	// Aceptamos un nuevo cliente
+	int socket_cliente;
+	socket_cliente = accept(socket_servidor, NULL, NULL);
+
+	uint32_t handshake;
+	uint32_t resultOk = 0;
+	uint32_t resultError = -1;
+
+	recv(socket_cliente, &handshake, sizeof(uint32_t), MSG_WAITALL);
+	if (handshake == 1)
+		send(socket_cliente, &resultOk, sizeof(uint32_t), NULL);
+	else
+		send(socket_cliente, &resultError, sizeof(uint32_t), NULL);
+
+	if (socket_cliente == -1) 	{
+		log_error(logger, "Error al aceptar un nuevo cliente");
+	} else {
+		log_info(logger, "Se conecto un cliente!");
+	}
+
+	return socket_cliente;
+}
+
+int recibir_operacion(int socket_cliente)
+{
+	int cod_op;
+	if (recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0) {
+		return cod_op;
+	} else {
+		close(socket_cliente);
+		return -1;
+	}
+}
+
+void *recibir_buffer(int *size, int socket_cliente)
+{
+	void *buffer;
+
+	recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
+	buffer = malloc(*size);
+	recv(socket_cliente, buffer, *size, MSG_WAITALL);
+
+	return buffer;
+}
+
+void recibir_mensaje(int socket_cliente)
+{
+	int size;
+	char *buffer = recibir_buffer(&size, socket_cliente);
+	free(buffer);
+}
+
+t_list *recibir_paquete(int socket_cliente)
+{
+	int size;
+	int desplazamiento = 0;
+	void *buffer;
+	t_list *valores = list_create();
+	int tamanio;
+
+	buffer = recibir_buffer(&size, socket_cliente);
+	while (desplazamiento < size)
+	{
+		memcpy(&tamanio, buffer + desplazamiento, sizeof(int));
+		desplazamiento += sizeof(int);
+		char *valor = malloc(tamanio);
+		memcpy(valor, buffer + desplazamiento, tamanio);
+		desplazamiento += tamanio;
+		list_add(valores, valor);
+	}
+	free(buffer);
+	return valores;
+}
