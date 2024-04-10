e8caf3c5d0999a286f27d53385f4e3ae9a1cf9c7
nicofishman <nicourifishman@gmail.com>
2024-04-10 18:04:32 +0000

    [KERNEL] - Lectura de config y creaición de logs

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,8 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/hello.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/string.h>
 
 int main(int argc, char* argv[]) {
-    decir_hola("Kernel");
+    // decir_hola("Kernel");
+
+    t_log* logger = log_create("kernel.log", "Kernel", 1, LOG_LEVEL_INFO);
+    t_config* config = config_create("kernel.config");
+
+    int puerto = config_get_int_value(config, "PUERTO_ESCUCHA");
+
+    char* stringParaLogger = string_from_format("Hola, soy un kernel! Puerto: %d", puerto);
+    log_info(logger, stringParaLogger);
+
     return 0;
 }
