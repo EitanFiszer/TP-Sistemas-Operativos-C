07559fa6cfa91460cff4bdb59d3fa57fccc88c08
nicofishman <nicourifishman@gmail.com>
2024-04-10 18:10:19 +0000

    [E/S] - Lectura de config y creación de logs

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -1,8 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/hello.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/string.h>
 
 int main(int argc, char* argv[]) {
-    decir_hola("una Interfaz de Entrada/Salida");
+    
+    t_log* logger = log_create("entradasalida.log", "Entrada_Salida", 1, LOG_LEVEL_INFO);
+    t_config* config = config_create("entradasalida.config");
+
+    int puerto = config_get_int_value(config, "PUERTO_MEMORIA");
+
+    char* stringParaLogger = string_from_format("Hola, soy una entrada y salida! Puerto: %d", puerto);
+    log_info(logger, stringParaLogger);
+
     return 0;
 }
