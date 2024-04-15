bd0ce535a5e4f09330820124bce1e0820706057f
nicofishman <nicourifishman@gmail.com>
2024-04-15 20:03:06 +0000

    [E/S] - configs y handshake a memoria y kernel

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <utils/hello.h>
+#include <utils/server.h>
+#include <utils/client.h>
 #include <commons/log.h>
 #include <commons/config.h>
 #include <commons/string.h>
@@ -13,6 +15,16 @@ int main(int argc, char* argv[]) {
 
     char* stringParaLogger = string_from_format("[EyS] Escuchando en el puerto: %d", puerto);
     log_info(logger, stringParaLogger);
+
+     // leemos las configs
+    char* ip_kernel = config_get_string_value(config, "IP_KERNEL");
+    char* ip_memoria = config_get_string_value(config, "IP_MEMORIA");
+    char* puerto_kernel = config_get_string_value(config, "PUERTO_KERNEL");
+    char* puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
+
+    //cliente se conecta al sevidor 
+    int resultHandshake = connectAndHandshake(ip_memoria, puerto_memoria, IO, "memoria", logger);
+    int resultHandshake = connectAndHandshake(ip_kernel, puerto_kernel, IO, "kernel", logger);
 
     return 0;
 }
