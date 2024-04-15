d897022e9dc6d2b7c51a25bfac1b1c9d3726250f
nicofishman <nicourifishman@gmail.com>
2024-04-15 20:01:48 +0000

    [KERNEL] - Config, inicio servidor y handshake a memoria

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <utils/hello.h>
+#include <utils/server.h>
 #include <commons/log.h>
 #include <commons/config.h>
 #include <commons/string.h>
@@ -15,6 +15,24 @@ int main(int argc, char* argv[]) {
 
     char* stringParaLogger = string_from_format("[KERNEL] Escuchando en el puerto: %d", puerto);
     log_info(logger, stringParaLogger);
+
+    // leemos las configs
+    char* ip_memoria = config_get_string_value(config, "IP_MEMORIA");
+    char* ip_cpu = config_get_string_value(config, "IP_CPU");
+    char* puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
+    char* puerto_escucha = config_get_string_value(config, "PUERTO_ESCUCHA");
+    char* puerto_cpu_dispatch = config_get_string_value(config, "PUERTO_CPU_DISPATCH");
+    char* puerto_cpu_interrupt = config_get_string_value(config, "PUERTO_CPU_INTERRUPT");
+
+    // creamos el servidor
+    int server_fd = iniciar_servidor(puerto_escucha, logger);
+
+    //cliente se conecta al sevidor 
+   // int resultHandshake = connectAndHandshake(ip_cpu, puerto_cpu_dispatch, KERNEL, "cpu", logger);
+   // int resultHandshake = connectAndHandshake(ip_cpu, puerto_cpu_interrupt, KERNEL, "cpu", logger);
+
+    int resultHandshake = connectAndHandshake(ip_memoria, puerto_memoria, KERNEL, "memoria", logger);
+
 
     return 0;
 }
