fdb626abd3becb026b8acf3a1ecd456690b6b722
Facundo Vilamowski <facundovilamowski@gmail.com>
2024-05-10 18:47:58 +0000

    [MEMORIA]_Lectura_de_instrucciones

--- memoria/src/memoria-utils/procesos.c
+++ memoria/src/memoria-utils/procesos.c
@@ -16,24 +16,15 @@ char** leer_archivo(const char *nombre_archivo, t_log* logger, int* num_lineas) 
         log_info(logger, "No se pudo abrir el archivo");
         exit(1);
     } else {
-        log_info(logger, "Archivo abierto correctamente");
-        char buffer[1024]; // Tamaño máximo de una línea, ajusta según necesidad
-        char** lineas = NULL;
-        int capacidad = 0;
+        log_info(logger, "Archivo abierto");
+        char linea[32];
+        char** lineas = malloc(1000 * sizeof(char*)); //Puse 1000 lineas de tamanio
         int contador = 0;
 
-        while (fgets(buffer, sizeof(buffer), archivo)) {
-            if (contador >= capacidad) {
-                capacidad += 10; // Ajusta según necesidad
-                lineas = realloc(lineas, capacidad * sizeof(char*));
-                if (!lineas) {
-                    // Error al asignar memoria
-                    fclose(archivo);
-                    exit(1);
-                }
-            }
-            lineas[contador] = strdup(buffer);
+        while (fgets(linea, sizeof(linea), archivo)) {
+            lineas[contador] = strdup(linea);
             contador++;
+            if (contador >= 1000 || feof(archivo)) { break; }
         }
 
         fclose(archivo);
