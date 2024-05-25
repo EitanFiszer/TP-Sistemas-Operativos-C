64c1f1a51d479243a3996fe674f5dfe8ae6fd3f2
EitanFiszer <eitandfiszer@gmail.com>
2024-05-25 18:52:34 +0000

    [E/S]-Correcion de errores

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -7,8 +7,10 @@
 #include <commons/log.h>
 #include <commons/config.h>
 #include <commons/string.h>
-//#include <IO-utils/Operations.h>
 #include <utils/constants.h>
+#include <string.h>
+
+
 
 int main(int argc, char* argv[]) {
     t_log* logger = log_create("entradasalida.log", "Entrada_Salida", 1, LOG_LEVEL_INFO);
@@ -24,40 +26,42 @@ int main(int argc, char* argv[]) {
 
     //int resultHandshakeMemoria = connectAndHandshake(ip_memoria, puerto_memoria, IO, "memoria", logger);
 
-    payload_intruccion_io x = malloc(sizeof(payload_intruccion_io));
+    payload_intruccion_io* x = malloc(sizeof(payload_intruccion_io));
 
     //Se conecta a kernel y le envia las interfaces
     int resultHandshakeKernell = connectAndHandshake(ip_kernel, puerto_kernel, IO, "kernel", logger); 
-    t_paquete *paq = crear_paquete();
+    t_paquete* paq = crear_paquete();
     t_paquete_entre* paquete=malloc(sizeof(t_paquete_entre));  
     t_payload_io_gen_sleep* payload = malloc(sizeof(t_payload_io_gen_sleep));
     
-    payload->interfaz = x.op;
+    payload->interfaz = x->op;
     paquete->payload = payload;
 
     agregar_a_paquete(paq, paquete, sizeof(t_paquete_entre));
-    enviar_paquete(paquete,resultHandshakeKernell)
+    enviar_paquete(paquete,resultHandshakeKernell);
     log_info(logger, "PAQUETE CREADO Y ENVIADO A KERNEL");
     eliminar_paquete(paq);
     free(paquete);
     
+
+
     //espera a recibir una instrucción y la ejecuta
     while(1){
         t_list* paq = rebicir_paquete(resultHandshakeKernell);
         t_paquete_entre* paquete_dispatch = list_get(paquete, 0);
-        OP_CODES_ENTRE op = paquete_dispatch->operacion;
+        OP_CODES_ENTRE* op = paquete_dispatch->operacion;
 
         if (op !== INSTRUCCION_IO) {
             log_error(logger, "Operacion no soportada");
             break;
         }
 
-        op_io operacionRecibida = paquete_dispatch->payload;
+        op_io* operacionRecibida = paquete_dispatch->payload;
 
-        switch(operacionRecibida.op_code){
+        switch(operacionRecibida->op_code){
             case IO_GEN:
-                char* interfaz = operacionRecibida.interfaz;
-                int tiempo = operacionRecibida.tiempo;
+                char* interfaz = operacionRecibida->interfaz;
+                int tiempo = operacionRecibida->tiempo;
                 
                 log_info(logger,"Operacion: <IO_GEN_SLEEP>");
                 sleep(tiempo_unidad_trabajo / 1000 * tiempo);
