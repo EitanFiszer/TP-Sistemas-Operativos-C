58c3a1647d012c03e5b9923237317a25b5de35a5
nicofishman <nicourifishman@gmail.com>
2024-07-26 16:21:15 +0000

    [I/O] - Fix para build

--- entradasalida/src/bitmap.c
+++ entradasalida/src/bitmap.c
@@ -8,14 +8,14 @@ t_bitmap* crear_bitmap(int block_count, const char* path) {
     t_bitmap* bitmap = malloc(sizeof(t_bitmap));
     
     t_bitarray* bits = bitarray_create_with_mode(NULL, block_count, MSB_FIRST);
-    bitmap->bits =  bits // Inicializa todos los bits a 0
+    bitmap->bits =  bits; // Inicializa todos los bits a 0
     bitmap->block_count = block_count;
     bitmap->path = strdup(path);
     return bitmap;
 }
 
-int obtener_bloque_libre(t_bitarray* bitmap) {
-    t_bitarray* bits = bitmap->bits  
+int obtener_bloque_libre(t_bitmap* bitmap) {
+    t_bitarray* bits = bitmap->bits;  
     int max = bitarray_get_max_bit(bits);
 
     for (int i = 0; i < max; i++) {
@@ -29,14 +29,14 @@ int obtener_bloque_libre(t_bitarray* bitmap) {
 void marcar_bloque(t_bitmap* bitmap, int bloque){
     int max = bitarray_get_max_bit(bitmap->bits);
     if (bloque >= 0 && bloque < max){
-        bitarray_set_bit(bitmap->bits, bloque)
+        bitarray_set_bit(bitmap->bits, bloque);
     }
 }
 
 
 void desmarcar_bloque(t_bitmap* bitmap, int bloque){
     int max = bitarray_get_max_bit(bitmap->bits);
-    if (bloque >= 0 && bloque < max){
-        bitarray_clean_bit(bitmap->bits, bloque)
+    if (bloque >= 0 && bloque < max) {
+        bitarray_clean_bit(bitmap->bits, bloque);
     }
 }
